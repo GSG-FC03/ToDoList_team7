7033d552ae4efc770793bb2a5c73043b3ca1b127
abdulrahman-2020 <akalousa@students.iugaza.edu.ps>
2021-07-18 12:06:24 +0300

    Inserting user-entered values in local storage & getting from local storage.
    Deleting particular li list from ol and local storage
    Relates #17

--- javascript/main.js
+++ javascript/main.js
@@ -5,6 +5,20 @@ let deleteAllBtn = document.querySelector(".del button");
 let taskcount = document.querySelector(".listNum span"); //numbers of task
 let noTaskMsg = document.querySelector(".message");
 
+//Local storage def.
+btnadd.addEventListener("click", function () {
+  inputBtnaddVal = input.value;
+  let webtask = localStorage.getItem("New Todo");
+  if (webtask == null) {
+    taskObj = [];
+  } else {
+    taskObj = JSON.parse(webtask);
+  }
+  taskObj.push(inputBtnaddVal);
+  localStorage.setItem("New Todo", JSON.stringify(taskObj));
+  showTasks(); //calling showTasks
+});
+
 // //make foucs on Input filed
 window.onload = function () {
   input.focus();
@@ -13,22 +27,17 @@ window.onload = function () {
 // //Adding new task to do list
 
 btnadd.onclick = function () {
-
   //check if the input filed is empty of not >print no value
   if (input.value === "") {
-    console.log("No value");//test
-  } 
-  
-  else {
+    console.log("No value"); //test
+  } else {
     noTaskMsg.remove();
 
     const elementList = document.querySelector(".elementList .list");
- 
-
     const newlist = document.createElement("li");
     const deletbtn = document.createElement("button");
     const editbtn = document.createElement("button");
-   
+
     deletbtn.innerHTML = '<i class="fas fa-trash"></i>';
     editbtn.innerHTML = '<i class="fas fa-edit"></i>';
 
@@ -42,19 +51,32 @@ btnadd.onclick = function () {
     deletbtn.addEventListener("click", function () {
       const parent = this.parentNode;
       parent.remove();
+      deleteTask(parent);
     });
 
     input.value = "";
   }
+}; //end add function
+//deleting Task from local storage
+function deleteTask(index) {
+  let webtask = localStorage.getItem("New Todo");
+  taskObj = JSON.parse(webtask);
+  taskObj.splice(index, 1);
+  localStorage.setItem("New Todo", JSON.stringify(taskObj));
+}
 
+// function showTasks(){
+//   let webtask = localStorage.getItem("New Todo");
+//   if (webtask == null) {
+//     taskObj = [];
+//   } else {
+//     taskObj = JSON.parse(webtask);
+//   }
+//   let newLiTag = '';
+//   taskObj.forEach((element, index) => {
+//     newLiTag += `<li>${element} <button><i class="fas fa-edit"></i></button><button><i onclick="deleteTask(${index})" class="fas fa-trash"></i></button></li>`;
 
-}//end add function 
+//   });
+//   newlist.innerHTML = newLiTag;
 
-
-
-
-
-
-
-
-
+// }
